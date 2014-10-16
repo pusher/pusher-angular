b01274b8c8927c5f3091039e9bcf97c9a29e695c
Hamilton Chapman <hamchapman@gmail.com>
2014-10-16 16:30:04 +0100

    Added preprocessor for coverage

--- traivs_karma.conf.js
+++ traivs_karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'lib/pusher-angular.js': 'coverage'
     },
 
     // test results reporter to use
